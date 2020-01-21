991f140766abcc4c381f4b3a1238939a2d813b9f
Andrew Rohl <andrew.rohl@curtin.edu.au>
2020-01-21 23:29:24 +0800

    Added full forecast as submenu, changed colours to improve readability and made string delimiter consistent

--- weather.py
+++ weather.py
@@ -21,41 +21,41 @@ sum_data = sum.summary()
 degree = unit
 print(str(value)+ unit + "C")
 print("---")
-print (location["name"] + " Observations | color=black")
-print ("Current: " + str(value) + unit + "C" + " | color=dimgray")
+print (location["name"] + " Observations | color=royalblue")
+print ("Current: " + str(value) + unit + "C" + " | color=black")
 (label, value, unit) = sum_data["temp_feels_like"]
-print ("Feels like: " + str(value) + unit + "C" + " | color=dimgray")
+print ("Feels like: " + str(value) + unit + "C" + " | color=black")
 (label, value, unit) = sum_data["temp_now"]
 if label == "Max":
-    print ("Maximum: " + str(value) + unit + "C" + " | color=dimgray")
+    print ("Maximum: " + str(value) + unit + "C" + " | color=black")
 (label, value, unit) = sum_data["precis"]
-print ("Conditions: " + value[:-1] + " | color=dimgray")
+print ("Conditions: " + value[:-1] + " | color=black")
 
 weather = api.WeatherApi(search=my_location, debug=0)
 obs = weather.observations()
 forecast = weather.forecasts_daily()
-print("Humidity: " + str(obs["humidity"]) + "% | color=dimgray")
-print("Wind: " + obs["wind"]["direction"] + " @ " + str(obs["wind"]["speed_kilometre"]) + " kmh | color=dimgray" )
+print("Humidity: " + str(obs["humidity"]) + "% | color=black")
+print("Wind: " + obs["wind"]["direction"] + " @ " + str(obs["wind"]["speed_kilometre"]) + " kmh | color=black" )
 sunrise = forecast[0]["astronomical"]["sunrise_time"]
 sunset = forecast[0]["astronomical"]["sunset_time"]
-print("Sunrise: " + convert_date(sunrise).strftime("%-I" + ":" + "%-M %p") + " | color=dimgray")
-print("Sunset: " + convert_date(sunset).strftime("%-I" + ":" + "%-M %p") + " | color=dimgray")
+print("Sunrise: " + convert_date(sunrise).strftime("%-I" + ":" + "%-M %p") + " | color=black")
+print("Sunset: " + convert_date(sunset).strftime("%-I" + ":" + "%-M %p") + " | color=black")
 if (obs["rain_since_9am"] > 0):
-	print("Rain since 9 am: " + str(obs["rain_since_9am"]) + " mm | color=dimgray")
+	print("Rain since 9 am: " + str(obs["rain_since_9am"]) + " mm | color=black")
 print("---")
-print (location["name"] + " Forecast | color=black")
+print (location["name"] + " Forecast | color=royalblue")
 forecast.pop(0)
 for f in forecast:
-    if f['temp_min'] is None:
-        temp_min = '--'
+    if f["temp_min"] is None:
+        temp_min = "--"
     else:
-        temp_min = f['temp_min']
+        temp_min = f["temp_min"]
 
-    if f['temp_max'] is None:
-        temp_max = '--'
+    if f["temp_max"] is None:
+        temp_max = "--"
     else:
-        temp_max = f['temp_max']
-    date = convert_date(f['date'])
+        temp_max = f["temp_max"]
+    date = convert_date(f["date"])
     formatted_date = date.strftime("%A")
-    print(formatted_date + " Min: " + str(temp_min) + degree + "C, Max: " + str(temp_max)  + degree + "C, " + f['short_text'][:-1] + " | color=dimgray")
-
+    print(formatted_date + " Min: " + str(temp_min) + degree + "C, Max: " + str(temp_max)  + degree + "C, " + f["short_text"][:-1] + " | color=black")
+    print("--" + f["extended_text"][:-1] + " | size=12 color=black")
