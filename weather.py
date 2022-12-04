270b6d4a9b64ae97947acc20e52c8cd6a747ec1f
Andrew Rohl <andrew.rohl@curtin.edu.au>
2022-12-04 18:32:25 +0800

    Add SF Symbols to menubar to show conditions

--- weather.py
+++ weather.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env -P/opt/local/bin/:/Users/andrew/opt/anaconda3/bin python3
+#!/usr/bin/env -P/Users/andrew/.asdf/shims/:/Users/180341c/.asdf/shims/:/opt/local/bin/:/Users/andrew/opt/anaconda3/bin:/Users/andrew/anaconda3/bin:/Users/180341c/opt/anaconda3/bin/    python3
 
 # <bitbar.title>Perth Weather</bitbar.title>
 # <bitbar.version>v0.9</bitbar.version>
@@ -15,7 +15,7 @@ from dateutil.parser import parse
 def convert_date(date):
   return (parse(date).astimezone())
 
-my_location = 'perth+wa'
+my_location = 'kensington+wa'
 sum = summary.Summary(search=my_location)
 location = sum.location
 # check if the search produced a result (other methods will also return None if the search fails).
@@ -25,9 +25,28 @@ if location is None:
 	print(f"Location not found: {my_location}")
 	sys.exit()
 sum_data = sum.summary()
+(label, value, unit) = sum_data["precis"]
+conditions = value[:-1]
+if 'sunny'.casefold() in conditions.casefold():
+    symbol = ":sun.max.fill:"
+elif 'cloud'.casefold() in conditions.casefold():
+    symbol = ":cloud.fill:"
+elif 'clear'.casefold() in conditions.casefold():
+    symbol = ":moon.fill:"
+elif 'shower'.casefold() in conditions.casefold():
+    symbol = ":cloud.rain.fill:"
+elif 'rain'.casefold() in conditions.casefold():
+    symbol = ":cloud.heavyrain.fill:"
+elif 'storm'.casefold() in conditions.casefold():
+	symbol = ":cloud.bolt.fill:"
+elif 'smoke'.casefold() in conditions.casefold():
+	symbol = ":smoke.fill:"
+
+else:
+    symbol = ":exclamationmark.triangle:"
 (label, temp, unit) = sum_data['current_temp']
 degree = unit
-print(f"{temp:.1f}{unit}C")
+print(f"{symbol} {temp:.1f}{unit}C")
 time_now = datetime.now().strftime('%-I:%M %p')
 print("---")
 print (f"{location['name']} Observations @ {time_now} | color=royalblue")
@@ -66,14 +85,14 @@ for f in forecast:
 		temp_max = "--"
 	else:
 		temp_max = f['temp_max']
-   
+
 	date = convert_date(f['date'])
-	formatted_date = date.strftime('%A')
+	formatted_date = date.strftime('%a')
 	if f['short_text'] is not None:
-	  print(f"{formatted_date} Min: {temp_min}{degree}C, Max: {temp_max}{degree}C, {f['short_text'][:-1]} | color=black")
+	  print(f"{formatted_date} :thermometer.snowflake: {temp_min}{degree}C, :thermometer.sun.fill: {temp_max}{degree}C, {f['short_text'][:-1]} | color=black")
 	if f['extended_text'] is not None:
 		print(f"--{f['extended_text'][:-1]} | size=12 color=black")
-	
+
 print("---")
 print("Rain Radar | color=royalblue")
 print(location["name"] + " | color=black href=http://www.bom.gov.au/products/IDR703.loop.shtml#skip")
